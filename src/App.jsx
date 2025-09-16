12d8764abcd47113bcd39c59a61ee9c31fa2a39a
Hackerfairy <remydeklein@gmail.com>
2025-09-16 18:14:37 +0200

    feat: add split layout - controls on left, phone mockup on right

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import SceneRoot from './components/Scene/SceneRoot'
+import ControlPanel from './components/UI/ControlPanel'
 import useSceneStore from './store/useSceneStore'
 import './App.css'
 
@@ -20,45 +21,55 @@ function App() {
   }, [addModel])
   
   return (
-    <div className="phone-mockup">
-      {/* Phone frame */}
-      <div className="phone-frame">
-        {/* Background */}
-        <div className="phone-background">
-          {/* Quiz interface overlay */}
-          <div className="quiz-overlay">
-            <div className="quiz-modal">
-              <div className="quiz-header">
-                <div className="quiz-title">Quiz</div>
-                <div className="quiz-points">+45</div>
-                <div className="coin-icon">🪙</div>
-              </div>
-              <div className="quiz-progress">
-                <div className="progress-text">Question 1 of 5</div>
-                <div className="time-left">19s left</div>
-              </div>
-              <div className="progress-bars">
-                <div className="progress-bar active"></div>
-                <div className="progress-bar"></div>
-              </div>
-              <div className="question-text">
-                Who was one of the headliners in the 2024 edition of Worlds Away
-              </div>
-              <div className="answer-options">
-                <div className="answer-option">The Weekend & Post Malone</div>
-                <div className="answer-option selected">
-                  The Swedish House Mafia
-                  <div className="answer-badge">My answer ✓</div>
+    <div className="app-layout">
+      {/* Left side - Control Panel */}
+      <div className="controls-side">
+        <ControlPanel />
+      </div>
+      
+      {/* Right side - Phone Mockup */}
+      <div className="mockup-side">
+        <div className="phone-mockup">
+          {/* Phone frame */}
+          <div className="phone-frame">
+            {/* Background */}
+            <div className="phone-background">
+              {/* Quiz interface overlay */}
+              <div className="quiz-overlay">
+                <div className="quiz-modal">
+                  <div className="quiz-header">
+                    <div className="quiz-title">Quiz</div>
+                    <div className="quiz-points">+45</div>
+                    <div className="coin-icon">🪙</div>
+                  </div>
+                  <div className="quiz-progress">
+                    <div className="progress-text">Question 1 of 5</div>
+                    <div className="time-left">19s left</div>
+                  </div>
+                  <div className="progress-bars">
+                    <div className="progress-bar active"></div>
+                    <div className="progress-bar"></div>
+                  </div>
+                  <div className="question-text">
+                    Who was one of the headliners in the 2024 edition of Worlds Away
+                  </div>
+                  <div className="answer-options">
+                    <div className="answer-option">The Weekend & Post Malone</div>
+                    <div className="answer-option selected">
+                      The Swedish House Mafia
+                      <div className="answer-badge">My answer ✓</div>
+                    </div>
+                    <div className="answer-option">Tiësto</div>
+                    <div className="answer-submit">Submit</div>
+                  </div>
                 </div>
-                <div className="answer-option">Tiësto</div>
-                <div className="answer-submit">Submit</div>
+              </div>
+              
+              {/* AR 3D Model Area */}
+              <div className="ar-model-area">
+                <SceneRoot />
               </div>
             </div>
-          </div>
-          
-          {/* AR 3D Model Area */}
-          <div className="ar-model-area">
-            <SceneRoot />
           </div>
         </div>
       </div>
